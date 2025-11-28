bacd9ddd1882cf46bf9f388ca7a7fd78b17bfe25
Mikel Ken <mayki6866@gmail.com>
2025-11-27 20:39:31 -0400

    config

--- resources/js/Components/PageCounter.vue
+++ resources/js/Components/PageCounter.vue
@@ -5,7 +5,7 @@ import axios from "axios";
 const visitCount = ref(0);
 const pageName = ref("");
 
-const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:8000";
+// const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:8000";
 
 onMounted(async () => {
     try {
