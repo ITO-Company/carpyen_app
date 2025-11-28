890dd38b55825792e8a3bba412835eddc65731ea
Mikel Ken <mayki6866@gmail.com>
2025-11-27 21:09:58 -0400

    config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig(({ mode }) => ({
-    base: "/inf513/grupo03sa/proyecto2/carpyen_app/public/",
+    base: "/inf513/grupo03sa/proyecto2/carpyen_app/public/build/",
     plugins: [
         laravel({
             input: "resources/js/app.js",
